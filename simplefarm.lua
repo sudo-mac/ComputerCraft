81623f26656b3e51ce46b3b8b5a13dacbc394f7d
PaperMushrooms <dreems2reality@gmail.com>
2026-02-07 18:28:34 -0500

    Update

--- simplefarm.lua
+++ simplefarm.lua
@@ -1,18 +1,54 @@
-local function simplefarm()
-	local inspected, data
-	local info
+local function sort()
+	slot = 0
 
+	repeat
+		slot = slot + 1
+		turtle.select(slot)
+		item = turtle.getItemDetail()
+
+		if item ~= nil and item.name == "minecraft:wheat_seeds" then
+			turtle.transferTo(1)
+		elseif item ~= nil and item.name == "minecraft:beetroot_seeds" then
+			turtle.transferTo(2)
+		elseif item ~= nil and item.name == "minecraft:wheat" then
+			turtle.transferTo(13)
+		elseif item ~= nil and item.name == "minecraft:potato" then
+			turtle.transferTo(14)
+		elseif item ~= nil and item.name == "minecraft:beetroot" then
+			turtle.transferTo(15)
+		end
+	until slot >= 16
+
+	turtle.select(1)
+end
+
+local function harvest()
+	inspected, data = turtle.inspectDown()
+
+	if inspected and data.name == "minecraft:wheat" and data.state["age"] == 7 then
+		turtle.digDown()
+		sort()
+		turtle.select(1)
+		turtle.placeDown()
+	elseif inspected and data.name == "minecraft:beetroots" and data.state["age"] == 3 then
+		turtle.digDown()
+		sort()
+		turtle.select(2)
+		turtle.placeDown()
+	elseif inspected and data.name == "minecraft:potatoes" and data.state["age"] == 7 then
+		turtle.digDown()
+		sort()
+		turtle.select(14)
+		turtle.placeDown()
+	end
+end
+
+local function simplefarm()
 	turtle.up()
 	turtle.forward()
 
 	for i = 1, 5 do
-		inspected, data = turtle.inspectDown()
-
-		if inspected and data.state["age"] == 7 then
-			turtle.digDown()
-			turtle.placeDown()
-		end
-
+		harvest()
 		turtle.forward()
 	end
 
